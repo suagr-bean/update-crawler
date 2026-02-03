308306b8ba6a1a967ec040fd5434220747d243aa
suagr-bean <80102827+suagr-bean@users.noreply.github.com>
2026-02-03 06:44:50 +0000

    爬虫玩具

---
+++ crawler.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"fmt"
+)
+type Data struct {
+	Url string 
+	Etag string
+}
+func EtagAndSave(url string)bool{
+	 readetag,_:=os.ReadFile("etag.text")
+	data,err:= Crawler(url,string(readetag))
+	if err!=nil{
+		fmt.Println("爬取失败")
+		return false 
+	}
+	os.WriteFile("etag.text",[]byte(data.Etag),0644)//写入
+	return true
+}
+func Crawler(url string,etag string)(Data,error){
+   client:=&http.Client{}
+   data:=Data{}
+   req,_:= http.NewRequest("GET",url,nil)
+   req.Header.Set("If-None-Match",`etag`)
+   resp,err:=client.Do(req)
+  if err!=nil{
+	return data,err
+  }
+  defer resp.Body.Close()
+  update:=false
+  switch resp.StatusCode{
+  
+  case http.StatusNotModified:
+	 update=false
+  case http.StatusOK:
+	update=true 
+  }
+  if update==true{
+	etag:=resp.Header.Get("Etag")
+	if etag!=""{
+	 data:=Data{
+      Url:url,
+      Etag: etag,
+	}
+	return data,err
+} 
+  }
+  return data,err
+}
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"time"
+)
+func main(){
+
+	url:="https://wiwi.blog/blog/"
+	for {
+		fmt.Println("开始执行")
+    deal:= EtagAndSave(url)
+     if deal{
+		DealCmd(url)
+	 }
+	 time.Sleep(3*time.Minute)
+	}
+}
+func DealCmd (url string){
+	need:=fmt.Sprintf("termux-open %s",url)
+  cmd:= exec.Command("termux-notification","--title","网站有更新","--action",need,
+)
+   err:=cmd.Run()
+   if err!=nil{
+    fmt.Println("通知失败")
+   }
+}
